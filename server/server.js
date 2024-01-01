4e6ce617b05537bda1e9f693c481d03cb3ae0eaf
itksweb <itksweb@gmail.com>
2024-01-01 11:28:56 +0100

    adjusted for deployment

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const io = new Server(server, {
 });
 
 app.get("/", (req, res) => {
-  res.send("react chat app back end");
+  res.send("react chat app backend");
 });
 
 io.on("connection", (socket) => {
